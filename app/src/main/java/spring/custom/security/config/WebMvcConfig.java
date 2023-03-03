460a02b0ab1971a2459058eafb4df25d0f16eb75
leejinseok <ljs@bfactory.inc>
2023-03-03 10:47:18 +0900

    First commit

---
+++ app/src/main/java/spring/custom/security/config/WebMvcConfig.java
@@ -0,0 +1,16 @@
+package spring.custom.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import spring.custom.security.interceptor.AuthInterceptor;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new AuthInterceptor());
+    }
+
+}
